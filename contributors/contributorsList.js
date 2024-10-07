8757dd181daefb1dc7bc2cffe91dc486d503b99b
Rishabh000 <56599651+Rishabh000@users.noreply.github.com>
2024-10-07 17:03:23 -0400

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -2074,5 +2074,10 @@ contributors = [
     fullname: "Yajnesh Kumar",
     username: "https://github.com/yajneshkumar790",
   },
+  {
+    id: 428,
+    fullname: "Rishabh Ranka",
+    username: "https://github.com/Rishabh000",
+  },
   
 ];
